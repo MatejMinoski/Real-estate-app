0e49c654d6d56c8db76ce7989cef2c9c6b7b5f39
mmate <mmatej.minoski@gmail.com>
2024-01-19 02:20:42 +0100

    Complete sign in UI functionality

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -35,13 +35,14 @@ export const singIn=async(req,res,next)=>{
 const {email,password}=req.body;
 try {
   const validUser=await User.findOne({email})
+   if (!validUser) {
+     return next(errorHandler(404, "User not found"));
+   }
   const validPassword=bcrypt.compareSync(password,validUser.password)
   if(!validPassword){
     return next(errorHandler(401,"Wrong credentials"))
   }
-  if(!validUser){
-    return next(errorHandler(404,"User not found"))
-  }
+ 
   const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
   const { password: pass, ...rest } = validUser._doc;
   
